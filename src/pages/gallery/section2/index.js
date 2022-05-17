d579f69b7069c7d643d6320d05d171541339a25e
AfiNaufal97 <naufal.setiawan92@gmail.com>
2022-05-17 10:27:22 +0700

    update

--- src/pages/gallery/section2/index.js
+++ src/pages/gallery/section2/index.js
@@ -7,7 +7,7 @@ export default function index() {
     <div className="mb-[100px] mt-[100px] md:grid md:grid-cols-2  lg:grid-cols-2   xl:grid-cols-3">
       {JsonFile.geleryObject.map((e) => {
         return <Link to="/detail">
-          return (
+    
           <div className="max-w-sm mx-auto h-[400px]">
             <div className="bg-white shadow-lg shadow-slate-400 rounded-[30px] p-2 group">
               <div className="rounded-[30px] overflow-hidden">
@@ -21,7 +21,7 @@ export default function index() {
               <p className="px-[20px] text-[18px] font-bold">{e.name}</p>
             </div>
           </div>
-          );
+          
         </Link>;
       })}
     </div>
